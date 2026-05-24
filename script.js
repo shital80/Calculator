8c41e920a73de3d72308cc3cd59c2b45fbeb9012
Shital Kakasaheb Raut <rautsheetal80@gmail.com>
2026-05-24 09:57:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function appendValue(value) {
+  document.getElementById("display").value += value;
+}
+
+function clearDisplay() {
+  document.getElementById("display").value = "";
+}
+
+function calculate() {
+  let exp = document.getElementById("display").value;
+  document.getElementById("display").value = eval(exp);
+}
